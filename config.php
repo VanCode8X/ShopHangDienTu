a92707803dc7e40355b8676ab05af65af2e74494
vancode8x <vanthq@gmail.com>
2025-04-19 10:37:36 +0700

    Up first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'base' => '/PTIT24CNTT02/ss12/',
+    'assets' => '/PTIT24CNTT02/ss12/assets',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
